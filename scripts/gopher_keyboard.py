48e07be98e63c0bfd8b9301f2267392837ede29f
Nikita Boguslavskii <bognik3@gmail.com>
2022-12-16 18:23:37 -0500

    feat: Modify gopher_keyboard for pynput keyboard

--- scripts/gopher_keyboard.py
+++ scripts/gopher_keyboard.py
@@ -2,12 +2,27 @@
 
 import rospy
 
+import key_listener as kl
 from chest_mapping import chestMapping
+from arm_mapping import armMapping
 
-chestControls = chestMapping()
+
+def on_shutdown():
+    # Stop the chest motion
+    chestControls.stop_srv()
 
 if __name__ == '__main__':
+
+    
+    armControls = armMapping("/my_gen3", 0, -45, 90)
+    chestControls = chestMapping()
+
+    kl.keyboard_init()
+
     rospy.init_node("gopher_keyboard_control", anonymous=True)
 
     while not rospy.is_shutdown():
         pass
+
+    # Safe function if the node dies: only service calls or parameter setting (NO PUBLISHING)
+    rospy.on_shutdown(on_shutdown)
